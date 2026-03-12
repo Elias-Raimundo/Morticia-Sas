b0bea953c439b03b49fa99cc21ee338644bb33db
Elias <eliasraimundo2005@gmail.com>
2026-03-12 19:25:21 -0300

    Envio de PDF

--- sistema-pedidos/backend/src/utils/mailer.js
+++ sistema-pedidos/backend/src/utils/mailer.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
   port:587,
   secure:false,
   auth: {
